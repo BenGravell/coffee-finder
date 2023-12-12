36ecef9ff56fc3ece89ba87ec82836f6970901d3
Ben <42848659+BenGravell@users.noreply.github.com>
2023-12-12 02:30:07 -0500

    bugfix

--- html_utils.py
+++ html_utils.py
@@ -1,9 +1,6 @@
-from html import escape
-
-
 def tag_wrap(text: str, tag: str) -> str:
-    return f"<{tag}>{escape(text)}</{tag}>"
+    return f"<{tag}>{text}</{tag}>"
 
 
 def anchor_wrap(text: str, url: str) -> str:
-    return f'<a href="{escape(url)}" target="_blank" rel="noopener noreferrer">{escape(text)}</a>'
+    return f'<a href="{url}" target="_blank" rel="noopener noreferrer">{text}</a>'
